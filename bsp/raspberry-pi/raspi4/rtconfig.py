0ae99f1774d2d7550f53c59cb543157bbc576c57
刘恒言 <bingshan45@163.com>
2020-08-06 21:33:01 +0800

    包含tflmicro的完成工程,无main函数,用于尝试增加官方自带用例

--- bsp/raspberry-pi/raspi4/rtconfig.py
+++ bsp/raspberry-pi/raspi4/rtconfig.py
@@ -45,7 +45,7 @@ if PLATFORM == 'gcc':
     else:
         CFLAGS += ' -O2'
 
-    CXXFLAGS = CFLAGS
+    CXXFLAGS = '--std=c++11' + CFLAGS 
 
 DUMP_ACTION = OBJDUMP + ' -D -S $TARGET > rtt.asm\n'
 POST_ACTION = OBJCPY + ' -O binary $TARGET rtthread.bin\n' + SIZE + ' $TARGET \n'
